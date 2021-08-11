5493be7a0698327b1277b0612fce175de174e44d
Yannis Haismann <72089971+YannisHaismann@users.noreply.github.com>
2021-08-11 14:09:11 +0200

    Button favorite on track. Register or remove track_id in localstorage on click.

--- src/components/SongPage.vue
+++ src/components/SongPage.vue
@@ -1,8 +1,12 @@
 <template>
   <div v-if="song != null" class="song-informations">
-    <h1>{{ song.track }} - {{ song.artist }}</h1>
+    <h1>{{ song.track }} - {{ song.artist }} - {{ song.id_track }}</h1>
     <img :src="song.cover" :alt="song.album">
     <img :src="urlSongGif" alt="">
+    <div @click="addToFavorite" class="add-favorite-btn">
+      <img v-if="favorite"  src="../assets/favorite-true.png" alt="">
+      <img v-else src="../assets/favorite-false.png" alt="">
+    </div>
   </div>
 </template>
 
@@ -10,16 +14,37 @@
 
 export default {
   name: "SongPage",
+  beforeUpdate() {
+    this.checkIfIsFavorite();
+    console.log('update')
+  },
   data(){
     return{
-
+      favorite: false
     }
   },
   props: ['song', 'urlSongGif'],
   methods:{
-
+    // Add or remove from localstorage id of the track and change favorite btn in function
+    addToFavorite(){
+      if(this.favorite){
+        localStorage.removeItem(this.song.id_track);
+      }else{
+        localStorage.setItem(this.song.id_track, this.song.id_track);
+      }
+      this.checkIfIsFavorite();
+    },
+    // Check if the track is in localstorage and update variable favorite in function
+    checkIfIsFavorite(){
+      if(localStorage.getItem(this.song.id_track) == this.song.id_track){
+        this.favorite = true;
+      }else{
+        this.favorite = false;
+      }
+    }
   },
   computed:{
+
   }
 }
 </script>
